a0cf5c4a695c7d4bc4441eb1b517c82b0b4a2a2d
Lola <robitillel@outlook.com>
2022-06-26 15:40:29 -0400

    added playRound function

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+//computerPlay selects Rock, Paper, or Scissors at random to simulate an opponent.
 function computerPlay() {
    let rand = Math.floor(Math.random() * 3);
    const options = ["rock", "paper", "scissors"];
-   let answer = options[rand];
-   return answer;
+   let computerSelection = options[rand];
+   return computerSelection;
 }
-console.log(computerPlay);
+//playRound plays one round of rock paper scissors.
+function playRound(playerSelection, computerSelection) {
+   let result;
+   playerSelection = playerSelection.toLowerCase();
+   if (playerSelection == computerSelection) {
+      result = "tie";
+   } else if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "rock") {
+      result = "win"
+   } else {result = "lose"}
+   return result;
+}
