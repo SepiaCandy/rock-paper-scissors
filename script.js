233290bb7bd68fc1380568ae3577ee2741e9c4da
Lola <robitillel@outlook.com>
2022-06-26 17:16:21 -0400

    created game function

--- script.js
+++ script.js
@@ -3,16 +3,48 @@ function computerPlay() {
    let rand = Math.floor(Math.random() * 3);
    const options = ["rock", "paper", "scissors"];
    let computerSelection = options[rand];
+   //alert(computerSelection);
    return computerSelection;
+   
 }
 //playRound plays one round of rock paper scissors.
 function playRound(playerSelection, computerSelection) {
    let result;
-   playerSelection = playerSelection.toLowerCase();
+   
+   //playerSelection = playerSelection.toLowerCase();
+   
    if (playerSelection == computerSelection) {
-      result = "tie";
-   } else if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "rock") {
-      result = "win"
-   } else {result = "lose"}
+      result = "tied";
+   } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock")) {
+      result = "won";
+   } else {result = "lost";}
    return result;
 }
+
+function game() {
+   let playerScore = 0;
+   let computerScore = 0;
+for (let i = 0; i < 5; i++) {
+   let playerSelection = (window.prompt("Rock, Paper, or Scissors?"));
+   let computerSelection = computerPlay();
+   alert(computerSelection);
+   //alert(computerSelection);
+   playRound(playerSelection, computerSelection)
+   if (playRound(playerSelection, computerSelection) == "won") {
+      playerScore++;
+      alert("You won round " + (i+1) + ". The score is " + playerScore + " - " + computerScore);
+   
+   } else if (playRound(playerSelection, computerSelection) == "lost") {
+      computerScore++;
+      alert("You lost round " + (i+1) + ". The score is " + playerScore + " - " + computerScore);
+         
+   } else {
+      alert("Round " + (i+1) + " resulted in a tie. The score is " + playerScore + " - " + computerScore);
+      
+   }
+   
+}
+}
+//const playerSelection = "Rock";
+game()
+//alert(playRound(playerSelection, computerSelection));
